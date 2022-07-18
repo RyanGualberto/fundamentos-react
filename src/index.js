66dfacb9d75e0ee74c97898618f92689cc4f153b
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-18 09:26:44 -0300

    s2a14: working components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import  ReactDOM from 'react-dom'
 import React from 'react';
 import './index.css';
-
+import Primeiro from './components/basics/Primeiro';
 const tag = <h1>Ola</h1>;
 
 const el = document.getElementById("root");
 ReactDOM.render(
     <div>
-        { tag }
+        <Primeiro></Primeiro>
     </div>,
     el
     )
