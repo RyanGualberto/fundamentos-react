e4044127363eea7307bd26575c986537e79c5ed9
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-18 09:43:18 -0300

    working props in components

---
+++ src/components/basics/ComParametro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ComParametro(props){
+    return (
+        <div>
+            <h2>
+                {props.titulo}
+            </h2>
+            <h3>
+                Seja bem vindo
+            </h3>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import  ReactDOM from 'react-dom'
 import React from 'react';
-import './index.css';
 import Primeiro from './components/basics/Primeiro';
+import ComParametro from './components/basics/ComParametro';
+import './index.css';
 const tag = <h1>Ola</h1>;
 
 const el = document.getElementById("root");
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro titulo="segundo componente"/>
     </div>,
     el
     )
