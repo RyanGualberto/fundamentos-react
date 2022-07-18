38aef8c6ab804930447d7fd15702bce198f281d7
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-18 09:17:39 -0300

    s2a12: working JSX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import  ReactDOM from 'react-dom'
+import React from 'react';
+
+const tag = <h1>Ola</h1>;
 
 const el = document.getElementById("root");
-ReactDOM.render(" ola react!!!", el)
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+    el
+    )
